7c2be60ef90f3adc8c305bbe662d789866a58bd7
Antares-ProgramIng <140986762+Antares-ProgramIng@users.noreply.github.com>
2023-10-18 18:34:50 -0300

    Variables created

---
+++ js/validacion.js
@@ -0,0 +1,7 @@
+ NAME = document.getElementById("nombre")
+ PWD2 = document.getElementById("password2")
+ PWD1 = document.getElementById("password1")
+ REGBTN = document.getElementById("registerBtn")
+ EMAIL = document.getElementById("email")
+ LASTNAME = document.getElementById("apellido")
+
